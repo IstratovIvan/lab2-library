257efcc406ca3074cae7f8e93a03d2de8201929a
Your NameIstratovIvan <istratov.ivan33@gmail.com>
2025-03-12 16:17:56 +0300

    Initial commit with lab2 library

---
+++ CalCalcutor/CalCalcutor/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace CalCalcutor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.chkMale = new System.Windows.Forms.CheckBox();
+            this.chkFemale = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.chkMuscleGain = new System.Windows.Forms.CheckBox();
+            this.chkWeightLoss = new System.Windows.Forms.CheckBox();
+            this.chkMaintenance = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.tableLayoutPanel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel5, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.22222F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.77778F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.67148F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.32852F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.txtWeight, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.txtAge, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.txtHeight, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 1, 5);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(99, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 6;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(602, 319);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите ваш вес (кг)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите ваш возраст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введите ваш рост (см)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Выберите ваш пол";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Выберите цель расчета";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(199, 56);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtWeight.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(199, 109);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 22);
+            this.txtAge.TabIndex = 6;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(199, 162);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 22);
+            this.txtHeight.TabIndex = 7;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.00001F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.chkMale, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.chkFemale, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(199, 215);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(400, 47);
+            this.tableLayoutPanel3.TabIndex = 8;
+            // 
+            // chkMale
+            // 
+            this.chkMale.AutoSize = true;
+            this.chkMale.Location = new System.Drawing.Point(3, 3);
+            this.chkMale.Name = "chkMale";
+            this.chkMale.Size = new System.Drawing.Size(85, 20);
+            this.chkMale.TabIndex = 0;
+            this.chkMale.Text = "мужской";
+            this.chkMale.UseVisualStyleBackColor = true;
+            // 
+            // chkFemale
+            // 
+            this.chkFemale.AutoSize = true;
+            this.chkFemale.Location = new System.Drawing.Point(202, 3);
+            this.chkFemale.Name = "chkFemale";
+            this.chkFemale.Size = new System.Drawing.Size(84, 20);
+            this.chkFemale.TabIndex = 1;
+            this.chkFemale.Text = "женский";
+            this.chkFemale.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel4.Controls.Add(this.chkMuscleGain, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.chkWeightLoss, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.chkMaintenance, 2, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(199, 268);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(400, 48);
+            this.tableLayoutPanel4.TabIndex = 9;
+            // 
+            // chkMuscleGain
+            // 
+            this.chkMuscleGain.AutoSize = true;
+            this.chkMuscleGain.Location = new System.Drawing.Point(3, 3);
+            this.chkMuscleGain.Name = "chkMuscleGain";
+            this.chkMuscleGain.Size = new System.Drawing.Size(112, 20);
+            this.chkMuscleGain.TabIndex = 0;
+            this.chkMuscleGain.Text = "набор массы";
+            this.chkMuscleGain.UseVisualStyleBackColor = true;
+            // 
+            // chkWeightLoss
+            // 
+            this.chkWeightLoss.AutoSize = true;
+            this.chkWeightLoss.Location = new System.Drawing.Point(136, 3);
+            this.chkWeightLoss.Name = "chkWeightLoss";
+            this.chkWeightLoss.Size = new System.Drawing.Size(99, 20);
+            this.chkWeightLoss.TabIndex = 1;
+            this.chkWeightLoss.Text = "похудение";
+            this.chkWeightLoss.UseVisualStyleBackColor = true;
+            // 
+            // chkMaintenance
+            // 
+            this.chkMaintenance.AutoSize = true;
+            this.chkMaintenance.Location = new System.Drawing.Point(269, 3);
+            this.chkMaintenance.Name = "chkMaintenance";
+            this.chkMaintenance.Size = new System.Drawing.Size(118, 20);
+            this.chkMaintenance.TabIndex = 2;
+            this.chkMaintenance.Text = "поддержание";
+            this.chkMaintenance.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel5
+            // 
+            this.tableLayoutPanel5.ColumnCount = 3;
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel5.Controls.Add(this.btnCalculate, 1, 0);
+            this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(99, 328);
+            this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+            this.tableLayoutPanel5.RowCount = 2;
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(602, 119);
+            this.tableLayoutPanel5.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCalculate.Location = new System.Drawing.Point(93, 3);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(415, 53);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.tableLayoutPanel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.CheckBox chkMale;
+        private System.Windows.Forms.CheckBox chkFemale;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.CheckBox chkMuscleGain;
+        private System.Windows.Forms.CheckBox chkWeightLoss;
+        private System.Windows.Forms.CheckBox chkMaintenance;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ CalCalcutor/CalCalcutor/Form1.cs
@@ -0,0 +1,100 @@
+﻿using NutritionLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalCalcutor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void chkMuscleGain_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMuscleGain.Checked)
+            {
+                chkWeightLoss.Checked = false;
+                chkMaintenance.Checked = false;
+            }
+        }
+
+        private void chkWeightLoss_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkWeightLoss.Checked)
+            {
+                chkMuscleGain.Checked = false;
+                chkMaintenance.Checked = false;
+            }
+        }
+
+        private void chkMaintenance_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMaintenance.Checked)
+            {
+                chkMuscleGain.Checked = false;
+                chkWeightLoss.Checked = false;
+            }
+        }
+
+        private void chkMale_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMale.Checked)
+            {
+                chkFemale.Checked = false;
+            }
+        }
+
+        private void chkFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkFemale.Checked)
+            {
+                chkMale.Checked = false;
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получаем данные из полей
+                double height = double.Parse(txtHeight.Text);
+                int age = int.Parse(txtAge.Text);
+                double weight = 70; // Можно добавить поле для веса
+
+                // Определяем цель расчета
+                Goal goal;
+                if (chkMuscleGain.Checked)
+                    goal = Goal.MuscleGain;
+                else if (chkWeightLoss.Checked)
+                    goal = Goal.WeightLoss;
+                else
+                    goal = Goal.Maintenance;
+
+                // Определяем пол
+                Gender gender = chkMale.Checked ? Gender.Male : Gender.Female;
+
+                // Уровень активности (можно добавить поле для выбора)
+                double activityLevel = 1.55;
+
+                // Вычисление значений
+                var result = CalorieCalculator.CalculateDailyIntake(gender, weight, height, age, activityLevel, goal);
+
+                // Вывод результата
+                MessageBox.Show($"Калории: {result.Calories}\nБелки: {result.Proteins} г\nЖиры: {result.Fats} г\nУглеводы: {result.Carbs} г",
+                    "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода данных! Проверьте правильность значений.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
